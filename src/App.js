955fd2050d92a267ae4db4ba71592357cfef0508
jiwon9412 <80965224+jiwon9412@users.noreply.github.com>
2022-06-23 21:50:59 +0900

    INIT: footer, header 구조만 완성

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Row, Col } from 'antd';
 
 import Main from './components/pages/main';
+import Header from './components/templetes/Header';
+import Footer from './components/templetes/Footer';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/main" element={<Main />} />
-        </Routes>
+        <Row>
+          <Col span={24}>
+            <Header />
+          </Col>
+        </Row>
+        <Row>
+          <Col span={24}>
+            <Routes>
+              <Route path="/main" element={<Main />} />
+            </Routes>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={24}>
+            <Footer />
+          </Col>
+        </Row>
       </BrowserRouter>
     </div>
   );
